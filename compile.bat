59e3883e435f6c527804a222516535b67324429e
t-tang <7981810+t-tang@users.noreply.github.com>
2026-03-27 06:21:45 +0000

    Suppress additional warning

--- compile.bat
+++ compile.bat
@@ -3,9 +3,8 @@ IF "%1" == "" GOTO nomusic
 SET MUSIC=%1
 ECHO Compiling with %MUSIC%
 c:\portable\zx\zxbasic\zxbc.exe          ^
-   -M memory.txt -O0                     ^
    --output-format=tap --autorun --BASIC ^
-   -W160 -W170 -W190 -W110               ^
+   -W150 -W160 -W170 -W190 -W110         ^
    -D %MUSIC%                            ^
    --append-binary bin\PixelRowTable.bin ^
    --append-binary bin\Charset.bin       ^
@@ -15,7 +14,6 @@ GOTO :eof
 
 :nomusic
 c:\portable\zx\zxbasic\zxbc.exe          ^
-   -O0                                   ^
    --output-format=tap --autorun --BASIC ^
    -W160 -W170 -W190                     ^
    --append-binary bin\PixelRowTable.bin ^
